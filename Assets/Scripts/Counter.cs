26661d1b60303861dee551a5a6be54f21471fa78
dd4no <74273226+dd4no@users.noreply.github.com>
2022-12-28 19:28:27 -0500

    Count Enemy Hits

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -6,57 +6,33 @@ using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
-    public Text GreenCounterText;
-    public Text BlueCounterText;
-    public Text RedCounterText;
-    public Text PowerupCounterText;
-
-    public int GreenCount;
-    public int BlueCount;
-    public int RedCount;
-    public int PowerupCount;
-    public int TotalCount;
-
+    public GameManager gameManager;
 
     private void Start()
     {
-        //GreenCount = 0;
-        //BlueCount = 0;
-        //RedCount = 0;
-        //PowerupCount = 0;
-        //TotalCount = 0;
-
-        //GreenCounterText = Text.Find("GreenCounter");
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Green"))
+        if (CompareTag("Green"))
         {
-            GreenCount += 1;
-            GreenCounterText.text = "Count : " + GreenCount;
-            Debug.Log(GreenCount);
+            gameManager.UpdateCount("Green");
         }
-        if (other.CompareTag("Blue"))
+        if (CompareTag("Blue"))
         {
-            BlueCount += 1;
-            BlueCounterText.text = "Count : " + BlueCount;
-            Debug.Log(BlueCount);
-        }
-        if (other.CompareTag("Red"))
-        {
-            RedCount += 1;
-            RedCounterText.text = "Count : " + RedCount;
-            Debug.Log(RedCount);
-        }
-        if (other.CompareTag("Powerup"))
-        {
-            PowerupCount += 1;
-            PowerupCounterText.text = "Count : " + PowerupCount;
-            Debug.Log(PowerupCount);
-        }
+            gameManager.UpdateCount("Blue");
 
-        TotalCount = GreenCount + BlueCount + RedCount + PowerupCount;
-        Debug.Log(TotalCount);
+        }
+        if (CompareTag("Red"))
+        {
+            gameManager.UpdateCount("Red");
+
+        }
+        if (CompareTag("Powerup"))
+        {
+            gameManager.UpdateCount("Powerup");
+
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Counters
+    public int greenScore;
+    public int blueScore;
+    public int redScore;
+    public int powerupScore;
+    public int totalScore;
+
+    // Score Displays
+    public TextMeshProUGUI greenScoreText;
+    public TextMeshProUGUI blueScoreText;
+    public TextMeshProUGUI redScoreText;
+    public TextMeshProUGUI powerupScoreText;
+    public TextMeshProUGUI totalScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +29,39 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateCount(string enemy)
+    {
+        switch (enemy)
+        {
+            case "Green":
+                greenScore++;
+                greenScoreText.text = "Green Enemies: " + greenScore;
+                Debug.Log("Green: " + greenScore);
+                break;
+            case "Blue":
+                blueScore++;
+                blueScoreText.text = "Blue Enemies: " + blueScore;
+                Debug.Log("Blue: " + blueScore);
+
+                break;
+            case "Red":
+                redScore++;
+                redScoreText.text = "Red Enemies: " + redScore;
+                Debug.Log("Red: " + redScore);
+
+                break;
+            case "Powerup":
+                powerupScore++;
+                powerupScoreText.text = "Powerups: " + powerupScore;
+                Debug.Log("Green: " + powerupScore);
+
+                break;
+        }     
+
+        totalScore++;
+        totalScoreText.text = "Total Enemies: " + totalScore;
+
     }
 }
