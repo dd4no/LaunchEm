9057029c91ed1339ccb135c9b18d0cf50e4bf925
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2023-09-28 18:54:36 -0400

    Change Health Bar color on value decrease

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,33 +5,56 @@ using UnityEngine.UI;
 public class Shield : MonoBehaviour
 {
     public float hitPoints = 10;
-    public Slider sliderBar;
+
+    public Slider slider;
+    public Image sliderFill;
+
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI shieldText;
 
     // Start 
     void Start()
     {
         hitPoints = 10;
-        sliderBar.value = hitPoints;
+        slider.value = hitPoints;
     }
 
     // Update 
     void Update()
     {
+  
+        gameOverText.text = "";
+
         if (hitPoints < 0) 
         {
+            // Game Over Text
             gameOverText.text = "GAME OVER";
-        }
-        else
-        {
-            gameOverText.text = "";
-        }
-        
+            // GameOver();
+        }        
     }
 
     private void OnTriggerEnter(Collider other)
     {        
         hitPoints--;
-        sliderBar.value = hitPoints;
+        slider.value = hitPoints;
+
+        if (hitPoints <= 7 && hitPoints >= 4)
+        {
+            // Yellow
+            sliderFill.color = Color.yellow;
+        }
+
+        if (hitPoints <= 3 && hitPoints >= 1)
+        {
+            // Red
+            sliderFill.color = Color.red;
+        }
+
+        if (hitPoints == 0)
+        {
+            // Hide
+            slider.gameObject.SetActive(false);
+            shieldText.gameObject.SetActive(false);
+        }
     }
 }
